4048118ca5c7cf22fab4bf6ab8885348b904da73
hideki mori <nasust@gmail.com>
2020-01-26 11:38:15 +0900

    first commit

---
+++ flutter_bloc_counter/lib/bloc.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+
+class CounterBloc {
+  final _incrementController = StreamController<void>();
+  final _countController = StreamController<int>();
+
+  var _count = 0;
+
+  CounterBloc() {
+    _incrementController.stream
+      .map((v) => _count++)
+      .pipe(_countController);
+  }
+
+  Sink<void> get increment => _incrementController.sink;
+  Stream<void> get counter => _countController.stream;
+
+  void dispose() async {
+    await _incrementController.close();
+    await _countController.close();
+  }
+}
---
+++ flutter_bloc_counter/lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'bloc.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter BLoC Counter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  final _bloc = CounterBloc();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bloc.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            StreamBuilder<int>(
+                stream: _bloc.counter,
+                initialData: 0,
+                builder: (context, snapshot) {
+                  return Text(
+                    '${snapshot.data}',
+                    style: Theme.of(context).textTheme.display1,
+                  );
+                },),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _bloc.increment.add(null),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
